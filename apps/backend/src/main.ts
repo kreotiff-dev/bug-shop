8996dfbc0fa4e0db55d6f1a62aed88a82a4ac37d
Aleksandr Aleksandrov <hazg@mail.ru>
2023-06-20 15:26:21 +0300

    apm

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,24 @@
  * This is only a minimal backend to get started.
  */
 
+// Add this to the VERY top of the first file loaded in your app
+const apm = require('elastic-apm-node').start({
+
+  // Override the service name from package.json
+  // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+  serviceName: 'Bug-Shop',
+
+  // Use if APM Server requires a secret token
+  secretToken: '',
+
+  // Set the custom APM Server URL (default: http://localhost:8200)
+  serverUrl: 'http://localhost:8200',
+
+  // Set the service environment
+  environment: 'production'
+})
+
+
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
