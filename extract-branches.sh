bbb48b5dafaad40749aeb3b8e1c0991f0db0912f
Aleksandr Aleksandrov <hazg@mail.ru>
2023-06-21 16:50:45 +0300

    up

--- extract-branches.sh
+++ extract-branches.sh
@@ -9,8 +9,8 @@ do
     FOLDER="../branches/$(cut -d'/' -f3 <<<"$BRANCH")"
     echo branch: !$BRANCH!
     echo folder: !$FOLDER!
-    git checkout $BRANCH
-    mkdir $FOLDER
+    git checkout $BRANCH &&
+    mkdir $FOLDER &&
     rsync -r --exclude '.git' ./ $FOLDER
 done
 git checkout main
