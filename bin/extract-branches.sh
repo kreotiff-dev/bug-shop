3df2cd5144029e1d7333849cca08d53d3338ac53
Aleksandr Aleksandrov <hazg@mail.ru>
2023-06-21 22:15:19 +0300

    fetch all

--- bin/extract-branches.sh
+++ bin/extract-branches.sh
@@ -2,8 +2,7 @@
 
 # rm ../branches -r > /dev/null
 mkdir ../branches >> /dev/null
-git remote update
-git pull --all
+git fetch --all
 for BRANCH in $(git branch | awk '{if(NR>0)print}' | cut -c 3-) ;
 do
     FOLDER="../branches/$(cut -d'/' -f3 <<<"$BRANCH")"
