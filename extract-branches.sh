e52657002d6dcd6662def618190332f25314ae55
Aleksandr Aleksandrov <hazg@mail.ru>
2023-06-21 16:52:33 +0300

    cleanup

--- extract-branches.sh
+++ extract-branches.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-# CHANGE REPOSITORY NAME AT THE LINE BELOW!!!
 rm ../branches -r > /dev/null
 mkdir ../branches
-#  | grep remotes/origin | grep -v HEAD ####### | awk '{if(NR>0)print}'
 for BRANCH in $(git branch | awk '{if(NR>0)print}' | cut -c 3-) ;
 do
     FOLDER="../branches/$(cut -d'/' -f3 <<<"$BRANCH")"
-    echo branch: !$BRANCH!
-    echo folder: !$FOLDER!
+    echo Extract $BRANCH
     git checkout $BRANCH &&
     mkdir $FOLDER &&
     rsync -r --exclude '.git' ./ $FOLDER
