86453dd43fba881c054ebb532febd14d354612c7
Aleksandr Aleksandrov <hazg@mail.ru>
2023-06-22 16:07:45 +0300

    debug path

--- bin/start.sh
+++ bin/start.sh
@@ -5,10 +5,12 @@ for BRANCH in $(ls -1d $PWD/../branches/*) ;
 do
   NAME=$(basename $BRANCH)
   cd $BRANCH
+  echo "*** Now in $PWD"
   npm i
   nx build frontend --prod
   nx build
   pm2 start $BRANCH/dist/apps/backend/main.js --name $NAME
-  echo "***** RUNNING $NAME ******"
+  echo "*** RUNNING $NAME ***"
+  echo "*** Path $BRANCH"
 
 done
