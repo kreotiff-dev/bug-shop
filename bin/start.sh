2865305c7c210f57d86771e01fd841bb697a86f6
Aleksandr Aleksandrov <hazg@mail.ru>
2023-06-26 17:25:50 +0300

    start

--- bin/start.sh
+++ bin/start.sh
@@ -1,14 +1,37 @@
 #!/usr/bin/env bash
 
 mkdir ../branches -p
+
+CURRENT_DIR=$(pwd)
+
 for BRANCH in $(ls -1d $PWD/../branches/*) ;
 do
   NAME=$(basename $BRANCH)
   cd $BRANCH
+  echo "*** Now in $PWD"
   npm i
+  mkdir $BRANCH/dist/apps/backend/static/ -p
+  touch $BRANCH/dist/apps/backend/static/index.html # Bad build script?
   nx build frontend --prod
   nx build
-  mkdir ~/.forever/logs -p
-  pm2 start dist/apps/backend/main.js --name $NAME
-  echo "***** RUNNING $NAME ******"
+  cp static $BRANCH/dist/apps/frontend/api -r
+  pm2 start $BRANCH/dist/apps/backend/main.js --name $NAME
+  echo "*** RUNNING $NAME ***"
+  echo "*** Path $BRANCH"
+done
+
+cd $CURRENT_DIR
+echo "Return to $CURRENT_DIR"
+
+for BRANCH in $(ls -1d $PWD/../branches/*) ;
+do
+  cd $BRANCH
+  SOCKET="$PWD/server.socket"
+  file_to_wait=$SOCKET
+  echo "Waiting for $SOCKET"
+  while [ ! -S "$SOCKET" ]
+  do
+    sleep 1
+  done
+  chmod 777 $SOCKET
 done
